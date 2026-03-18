296e52e4c01da6d528969b3195e3b4c63f5d8c84
WhyNot <egorchik.270610@gmail.com>
2026-03-18 18:56:03 +0200

    Добавьте файлы проекта.

---
+++ CL/CL.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void line(char symb, int count)
+{//порожня функція
+    for (int i = 0; i < count; ++i) cout << symb;
+    cout << endl;
+}
+
+int main()
+{
+    system("chcp 1251>null");
+    cout << "Hello World!\n";
+
+    line('+', 10);
+    line('#', 15);
+    char symb = '@';
+    int count = 5;
+    line(symb, count);
+
+    return 0;
+}
